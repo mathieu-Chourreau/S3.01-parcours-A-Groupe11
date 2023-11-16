3945d2de54d7bf5af54b2a0ff847c119e522c642
Zaza Souleymen <szaza001@iutbayonne.univ-pau.fr>
2023-11-16 11:47:13 +0100

    creation de la base de la classe recette

---
+++ src/code/Recette.h
@@ -0,0 +1,16 @@
+/**
+ * @file Recette.h
+ * @author CHOURREAU,FERME,PIEL,ZAZA 
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-16
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef RECETTE_H
+#define RECETTE_H
+
+
+#endif
