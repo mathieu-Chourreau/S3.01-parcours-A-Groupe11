5661b155c07eb656aa449acc7d5a69162344e425
Nathan Piel <npiel@iutbayonne.univ-pau.fr>
2023-11-16 11:58:06 +0100

    création de la base de la classe Ingredient

--- src/code/Ingredient.h
+++ src/code/Ingredient.h
@@ -1,6 +1,39 @@
+/**
+ * @file Ingredient.h
+ * @author Chourreau Mathieu, Fermé Léo, Piel Nathan, Zaza Souleymen
+ * @brief 
+ * @version 0
+ * @date 2023-11-16
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
 #ifndef INGREDIENT_H
 #define INGREDIENT_H
+#include "Recette.h"
+#
 
+
+class Ingredient
+{
+private:
+    /* data */
+public:
+    Ingredient(/* args */);
+    ~Ingredient();
+};
+
+Ingredient::Ingredient(/* args */)
+{
+}
+
+Ingredient::~Ingredient()
+{
+}
 
 
 #endif
