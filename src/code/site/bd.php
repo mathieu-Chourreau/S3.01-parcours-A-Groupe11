8fe8921aaf575d059b0c4bd6a22fc2dda0b0db56
Mathieu Chourreau <mchourreau@iutbayonne.univ-pau.fr>
2023-12-18 21:05:55 +0100

    ajout du fichier qui permet de ce connecter à la base de donnee

---
+++ src/code/site/bd.php
@@ -0,0 +1,16 @@
+<?php
+// Connexion à la base de données MySQL
+$servername = "lakartxela.iutbayonne.univ-pau.fr"; // Remplacez ceci par votre nom de serveur
+$username = "szaza001_bd"; // Remplacez ceci par votre nom d'utilisateur
+$password = "szaza001_bd"; // Remplacez ceci par votre mot de passe
+$dbname = "szaza001_bd"; // Remplacez ceci par le nom de votre base de données
+
+// Connexion à la base de données avec MySQLi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
+}
+
+    ?>
