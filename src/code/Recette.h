2f5eeed6965ac6f3d2a6008f271ec96d7a5e85ec
Zaza Souleymen <szaza001@iutbayonne.univ-pau.fr>
2023-12-18 14:11:14 +0100

    test

--- src/code/Recette.h
+++ src/code/Recette.h
@@ -11,6 +11,33 @@
 
 #ifndef RECETTE_H
 #define RECETTE_H
+#include <iostream>
+#include <list>
+using namespace std;
 
+typedef pair<int/*a remplir*/, int> PoidIngredient;
+typedef list<PoidIngredient> lIngredient;
+class Recette
+{
+private:
+    string nom;
+    unsigned int prix;  
+    string temps;
+    unsigned difficulte;
+    string categorie;
+    string ustencile;
+    lIngredient mesIngredients;
+public:
+    Recette(/* args */);
+    ~Recette();
+};
+
+Recette::Recette(/* args */)
+{
+}
+
+Recette::~Recette()
+{
+}
 
 #endif
