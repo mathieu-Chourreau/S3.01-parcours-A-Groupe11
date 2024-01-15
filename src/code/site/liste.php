5338eabab8fdbb866b9f47a4f670d17ddce3c2da
Nathan Piel <npiel@iutbayonne.univ-pau.fr>
2024-01-15 11:05:56 +0100

    Rajout d'une liste pour stocker les variables de la page sale.php

--- src/code/site/liste.php
+++ src/code/site/liste.php
@@ -2,14 +2,24 @@
 
 session_start();
 
+if (!isset($_SESSION['ingredientsPreferencesPageSale'])) {
+    $_SESSION['ingredientsPreferencesPageSale'] = array();
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     foreach ($_POST as $nomIngredient => $valeur) {
-        $_SESSION['ingredientsPreferences'][$nomIngredient] = $valeur;
+        $_SESSION['ingredientsPreferencesPageSale'][$nomIngredient] = $valeur;
     }
 
     echo "<h2>Préférences des ingrédients (depuis la session) :</h2>";
     echo "<ul>";
     foreach ($_SESSION['ingredientsPreferences'] as $nomIngredient => $valeur) {
+        echo "<li>$nomIngredient : $valeur</li>";
+    }
+    echo "</ul>";
+    echo "<h2>Préférences des ingrédients (Page Sale) :</h2>";
+    echo "<ul>";
+    foreach ($_SESSION['ingredientsPreferencesPageSale'] as $nomIngredient => $valeur) {
         echo "<li>$nomIngredient : $valeur</li>";
     }
     echo "</ul>";
