1901c98a6560fbbaca4e5a268cef34cfa54f7a35
Fermé Léo <lferme001@iutbayonne.univ-pau.fr>
2024-01-30 16:38:56 +0100

    debut de la mise en page

--- src/code/site/index.php
+++ src/code/site/index.php
@@ -1,11 +1,22 @@
-<html lang="en">
+<!DOCTYPE html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <title>Edu'Cook</title>
 </head>
 <body>
-    <h1>Page d'accueil</h1>
+    <header>
+        <div class="logo">
+            <img src="image/logo.png" alt="Logo Edu'Cook">
+        </div>
+        <h1>Mon En-Tête</h1>
+    </header>
+    <nav id="barre_de_nav">
+
+    </nav>
+
 </body>
 </html>
