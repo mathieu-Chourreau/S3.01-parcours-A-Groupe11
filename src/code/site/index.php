69bf02e799b1f92756c71a8834ee9425d0e3efd0
Fermé Léo <lferme001@iutbayonne.univ-pau.fr>
2024-01-31 12:26:28 +0100

    avancemet input

--- src/code/site/index.php
+++ src/code/site/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-wvfX4Ftof8MkCpVgHaCmkidC8qDlUa/3N9zGQ5RG5Fv0VqDc5g5fE3WbmCUdBMYYBq2m8EhT6aD5J1LJ1yU1a0A==" crossorigin="anonymous" />
     <title>Edu'Cook</title>
 </head>
 <body>
@@ -12,12 +13,15 @@
         <div class="logo">
             <img src="image/logo.png" alt="Logo Edu'Cook">
         </div>
-        <h1>Mon En-Tête</h1>
     </header>
 
     <section class="fond">
-        <img id="img_background" src="image/fond_page_accueil.jpg">
-        <input type="text">
+        <img id="img_background" src="image/fond_page_accueil.png">
+        
+        <div class="search-container">
+            <input class = "recherche" type="text" placeholder = "rechercher">
+            <i id = "loupe" class="fas fa-search"></i>
+        </div>
     </section>
 </body>
 </html>
