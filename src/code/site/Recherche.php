cde8a1f7e10a75f868466fd670df9c5215a88326
Nathan Piel <npiel@iutbayonne.univ-pau.fr>
2024-02-12 15:19:25 +0100

    test

--- src/code/site/Recherche.php
+++ src/code/site/Recherche.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="recherche.css">
     <title>Edu'Cook</title>
 </head>
 <body>
     
-    
-    <div class="wrapper">
+    <header>
         <nav class="nav">
             <div class="logo">
                 <a href="index.html">
@@ -31,13 +31,35 @@
                 <button class="btn" id="registerBtn" onclick="register()">S'enregistrer</button>
             </div>
         </nav>
+    </header>
 
-        <div>
-            <form>
-                <input type="submit" name="" value="Toutes les recettes">
-                <input type="submit" name="" value="Végétarien">
-            </form>
-            <section>
+    <div>
+        <section class="e"></section>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <section>
+            <a href="couronne.php"><img class="section" src="1.jpg"></a>
+            <div class="texteSection">
+                <p>Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue (conjugaisons, construction et association des phrases…). Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku.</p>
+                <a href="couronne.php"><button>Voici nos couronnes</button></a>
+            </div>
+            <a href="couronne.php"><img class="section" src="1.jpg"></a>
+            <div class="texteSection">
+                <p>Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue (conjugaisons, construction et association des phrases…). Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku.</p>
+                <a href="couronne.php"><button>Voici nos couronnes</button></a>
+            </div>
             <a href="couronne.php"><img class="section" src="1.jpg"></a>
             <div class="texteSection">
                 <p>Un texte est une série orale ou écrite de mots perçus comme constituant un ensemble cohérent, porteur de sens et utilisant les structures propres à une langue (conjugaisons, construction et association des phrases…). Un texte n'a pas de longueur déterminée sauf dans le cas de poèmes à forme fixe comme le sonnet ou le haïku.</p>
@@ -57,8 +79,7 @@
             }
             */
             ?>
-            </section>
-        </div>
+        </section>
     </div>
     
     <footer class="footer">
