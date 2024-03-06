6b63ee523ff1601e6bbf0c56be230f43799381d3
Léo Fermé <lferme001@iutbayonne.univ-pau.fr>
2024-03-07 00:47:16 +0100

    page de résultat du formulaire fonctionnel

--- src/code/site/affichage_recette.php
+++ src/code/site/affichage_recette.php
@@ -1,6 +1,7 @@
 <?php include 'main.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,8 +11,9 @@
     <title>Détails de la recette</title>
     <link rel="stylesheet" href="resultat_formulaire.css">
 </head>
+
 <body>
-<nav id="nav">
+    <nav id="nav">
         <div id="divun">
             <a href="index.html"><img class="img_logo" src="image/logo.png"></a>
             <div class="hame">
@@ -37,57 +39,60 @@
             <button class="btn white-btn" id="loginBtn">Se connecter</button>
         </div>
     </nav>
-   
+
+    <div class="background"></div>
+    <h1 class="titre"> Voici nos meilleurs recettes pour vous ! </h1>
     <section class="s_recherche">
         <?php
         include 'bd.php';
 
         $topIngredients = array_slice($lRecettePoint, 0, 5);
 
-        var_dump($topIngredients);
-
-        /*$myString = implode(",", $topIngredients);
-
-       
+        //$myString = "'" . implode("','", array_column($topIngredients, 'recette')) . "'";
 
         $recetteValide = "SELECT r.identifiant, r.nom AS nom_recette, r.image AS imageR, r.instruction AS instruction, cr.gout AS categorie_recette, r.temps_min_ AS temps, r.niveau_difficulte AS dif
         FROM recette r
         JOIN appartenirrc a ON a.identifiantR = r.identifiant
         JOIN categorierecette cr ON a.identifiantC = cr.identifiant
-        WHERE r.nom IN (?)";
+        WHERE r.nom IN (?, ?, ?, ?, ?)";
 
         // Préparation de la requête
         $stmt = $conn->prepare($recetteValide);
 
         // Liaison des paramètres
-        $stmt->bind_param("s", $myString);
+        $stmt->bind_param("sssss",  $topIngredients[0]['recette'], $topIngredients[1]['recette'], $topIngredients[2]['recette'], $topIngredients[3]['recette'], $topIngredients[4]['recette']);
 
         $stmt->execute();
 
-        $result = $stmt->get_result();*/
+        $result = $stmt->get_result();
+        $indice = 0;
 
-        foreach ($topIngredients as $key => $recette) {
-            # code...
-            // Faire quelque chose avec chaque résultat
+            
+
+        while ($row = $result->fetch_assoc()) {
+            
             echo '<div class="card-grid">';
             echo '<div class="card">';
             echo '<div class="card-content">';
             echo '<div class="image-container">';
-            echo '<img src="' . $recette['imageR'] . '" class="imageCard">';
+            echo '<img src="' . $row['imageR'] . '" class="imageCard">';
             echo '</div>';
             echo '<div class="text-container">';
             echo '<div class="card-body">';
-            echo '<h4 class="card-title">' . $recette['nom_recette'] . '</h4>';
-            echo '<p class="typeP"><b>Catégorie : </b>' . $recette['categorie_recette'] . '</p>';
-            echo '<p class="card-text"><b>Description : </b>' . $recette['instruction'] . '</p>';
-            echo '<p class="card-text"><b>Niveau de difficulté : </b>' . $recette['dif'] . '</p>';
-            echo '<p class="card-text"><b>Temps : </b>' . $recette['temps'] . '</p>';
-            echo '<a href="details.php?recipeName=' . urlencode($recette['nom_recette']) . '&recipeCategory=' . urlencode($recette['categorie_recette']) . '&recipeDescription=' . urlencode($recette['instruction']) . '&recipeImageSrc=' . urlencode($recette['imageR']) . '" class="btn-details"><button class="btn white-btn">Voir les détails</button></a>';
+            echo '<p><b>Points : </b>' .$topIngredients[$indice]['point'] . '</p>';
+            echo '<h4 class="card-title">' . $row['nom_recette'] . '</h4>';
+            echo '<p class="typeP"><b>Catégorie : </b>' . $row['categorie_recette'] . '</p>';
+            echo '<p class="card-text"><b>Description : </b>' . $row['instruction'] . '</p>';
+            echo '<p class="card-text"><b>Niveau de difficulté : </b>' . $row['dif'] . '</p>';
+            echo '<p class="card-text"><b>Temps : </b>' . $row['temps'] . '</p>';
+            echo '<a href="details.php?recipeName=' . urlencode($row['nom_recette']) . '&recipeCategory=' . urlencode($row['categorie_recette']) . '&recipeDescription=' . urlencode($row['instruction']) . '&recipeImageSrc=' . urlencode($row['imageR']) . '" class="btn-details"><button class="btn white-btn">Voir les détails</button></a>';
             echo '</div>';
             echo '</div>';
             echo '</div>';
             echo '</div>';
             echo '</div>';
+
+            $indice = $indice + 1;
 
         }
         ?>
@@ -100,32 +105,33 @@
 </body>
 <script>
 
-const menuHamburger = document.getElementById("burger");
-        const navLinks = document.querySelector(".divdeux");
-        menuHamburger.addEventListener('click', () => { navLinks.classList.toggle('mobile-menu') });
+    const menuHamburger = document.getElementById("burger");
+    const navLinks = document.querySelector(".divdeux");
+    menuHamburger.addEventListener('click', () => { navLinks.classList.toggle('mobile-menu') });
 
-        var boutton = document.getElementsByClassName("bouttonveg");
-        function veg() {
-            var checkbox = document.getElementById("vegBouton");
-            var classCard = document.querySelectorAll(".card");
+    var boutton = document.getElementsByClassName("bouttonveg");
+    function veg() {
+        var checkbox = document.getElementById("vegBouton");
+        var classCard = document.querySelectorAll(".card");
 
-            if (checkbox.checked) {
-                for (var cards of classCard) {
-                    if (!(cards.querySelector(".typeP").textContent.includes("Végétarien"))) {
-                        cards.style.display = 'none';
-                    }
-                    if (cards.querySelector(".typeP").textContent.includes("Végétarien")) {
-                        cards.style.display = 'block';
-                    }
+        if (checkbox.checked) {
+            for (var cards of classCard) {
+                if (!(cards.querySelector(".typeP").textContent.includes("Végétarien"))) {
+                    cards.style.display = 'none';
                 }
-            } else {
-                for (var cards of classCard) {
+                if (cards.querySelector(".typeP").textContent.includes("Végétarien")) {
                     cards.style.display = 'block';
                 }
             }
+        } else {
+            for (var cards of classCard) {
+                cards.style.display = 'block';
+            }
         }
+    }
     const menuHamburger = document.getElementById("burger");
     const navLinks = document.querySelector(".divdeux");
     menuHamburger.addEventListener('click', () => { navLinks.classList.toggle('mobile-menu') });
 </script>
+
 </html>
