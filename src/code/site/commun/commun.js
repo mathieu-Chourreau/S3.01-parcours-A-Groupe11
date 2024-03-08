4828dc0f549afdc13eaa25af9f1b932eba5d4237
Nathan Piel <npiel@iutbayonne.univ-pau.fr>
2024-03-08 10:11:24 +0100

    nav connexion fonctionnel avec fichier commun

---
+++ src/code/site/commun/commun.js
@@ -0,0 +1,3 @@
+const menuHamburger = document.getElementById("burger");
+const navLinks = document.querySelector(".titreMenu");
+menuHamburger.addEventListener('click', () => { navLinks.classList.toggle('mobile-menu') });
